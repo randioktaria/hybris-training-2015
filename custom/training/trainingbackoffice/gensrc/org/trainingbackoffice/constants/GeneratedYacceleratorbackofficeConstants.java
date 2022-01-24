9d3b3e5ce5c3749a1161cefd2ee4c4bfcd7e9743
Randi Oktaria Rinanda <randioktaria96@gmail.com>
2022-01-24 09:20:00 +0700

    posting 1

---
+++ custom/training/trainingbackoffice/gensrc/org/trainingbackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jan 23, 2022, 4:16:23 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.trainingbackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "trainingbackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ custom/training/trainingfacades/gensrc/org/training/facades/constants/GeneratedTrainingFacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jan 23, 2022, 4:16:23 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.training.facades.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingFacadesConstants
+{
+	public static final String EXTENSIONNAME = "trainingfacades";
+	
+	protected GeneratedTrainingFacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
